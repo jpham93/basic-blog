482f35220804ec6cac81d37153020397df10c33e
James Pham <jamespham93@yahoo.com>
2020-02-19 17:30:00 -0800

    Integrate capacitor for desktop app capabilities

---
+++ client/src/components/NewBlog/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonInput, IonTextarea, IonLabel } from '@ionic/react';
+
+const NewBlog: React.FC = (props) => {
+  return(
+    <div>
+      <h2>New Blog</h2>
+      <form>
+
+      </form>
+    </div>
+  );
+};
+
+export default NewBlog;
